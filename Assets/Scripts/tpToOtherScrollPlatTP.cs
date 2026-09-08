f2743d105d1025ba193b16014a7150cb8eff9502
Stanley Liu <152922311+javameitser@users.noreply.github.com>
2026-09-07 23:48:40 -0400

    hello

--- Assets/Scripts/tpToOtherScrollPlatTP.cs
+++ Assets/Scripts/tpToOtherScrollPlatTP.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class tpToOtherScrollPlatTP : MonoBehaviour
 {
     // Start is called before the first frame update
 
     [SerializeField] GameObject otherScrollRegionTP;
+    PlayerMovement playerMovement;
     void Start()
     {
-        
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,13 @@ public class tpToOtherScrollPlatTP : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
+        Scene activeScene = SceneManager.GetActiveScene();
+
+        // 2. Create a list to hold the root objects
+        List<GameObject> rootObjects = new List<GameObject>();
+        activeScene.GetRootGameObjects(rootObjects);
+
+        // 3. Loop through root objects and their hierarchies
 
         float xShift = 0f;
         if(this.gameObject.name == "TPLeft")
@@ -30,17 +39,26 @@ public class tpToOtherScrollPlatTP : MonoBehaviour
             xShift = 1f;
         }
         Vector2 pos;
-        if (collision.CompareTag("Projectile"))
+
+        if (collision.CompareTag("Player"))    
         {
+            float valRShift = otherScrollRegionTP.transform.position.x + xShift * 50f - playerMovement.transform.position.x;
+
+            Debug.Log($"rootObjects: {rootObjects.Count}, valRShift: {valRShift}");
+            foreach (GameObject rootObj in rootObjects)
+            {
+                Debug.Log(rootObj.name);
+                if (rootObj.CompareTag("Monster") || rootObj.CompareTag("Projectile"))
+                {
+                    Debug.Log($"Shifting {rootObj.name} by {valRShift}");
+                    rootObj.transform.position = new Vector2(rootObj.transform.position.x + valRShift, rootObj.transform.position.y);
+                }
+            }
+
+
             collision.gameObject.transform.position = pos = new Vector2(otherScrollRegionTP.transform.position.x + xShift * 50f,collision.transform.position.y );
         }
-        if (collision.CompareTag("Monster"))
-        {
-            collision.gameObject.transform.position = pos = new Vector2(otherScrollRegionTP.transform.position.x + xShift * 50f, collision.transform.position.y);
-        }
-        if (collision.CompareTag("Player"))
-        {
-            collision.gameObject.transform.position = pos = new Vector2(otherScrollRegionTP.transform.position.x + xShift * 50f, collision.transform.position.y);
-        }
+
     }
+    
 }
